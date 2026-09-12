f0dc902f2de7761807c7a83739fd3ee8f61f2269
LuK1337 <priv.luk@gmail.com>
2026-09-12 23:59:46 +0200

    sm8850-common: Inherit hardware/oplus/rfs/rfs.mk
    Change-Id: Iec1d2bf612089c3d58b5496581982b053c4afdba

--- common.mk
+++ common.mk
@@ -350,6 +350,9 @@ PRODUCT_PACKAGES += \
     qspa_vendor.rc \
     vendor.qti.qspa-service
 
+# RFS
+$(call inherit-product, hardware/oplus/rfs/rfs.mk)
+
 # SecureElement
 ifneq ($(TARGET_IS_TABLET),true)
 PRODUCT_PACKAGES += \
